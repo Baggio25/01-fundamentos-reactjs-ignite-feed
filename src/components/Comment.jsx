a57fa8ed187015ad468caa80e2596f33b6618c77
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-14 17:01:59 -0300

    Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/26611668?v=4" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Rodrigo Baggio</strong>
+                            <time title='11 de maio as 11:13'  dateTime='2023-06-14 15:49:20'>
+                                Cerca de 1h atrás
+                            </time>        
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
