fddad385b6787c99ec3605f16469f6cc53b7aa9b
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-14 14:37:07 -0300

    First commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "@phosphor-icons/react";
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://i.pinimg.com/originals/bd/a5/be/bda5be61177acdb5fd46c3219f8b81a0.jpg"
+                />
+            
+            <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/26611668?v=4" 
+                    alt="profile_img"
+                />
+
+                <strong>Rodrigo Baggio</strong>
+                <span>Web Fullstack</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
