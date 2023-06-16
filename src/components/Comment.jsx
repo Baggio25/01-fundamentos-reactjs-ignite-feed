d11baed4040c8993927f8ed21d9d6ddc9a8e5ea0
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-16 16:27:58 -0300

    Like comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
 
 import { Avatar } from './Avatar';
@@ -6,9 +6,14 @@ import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -32,9 +37,9 @@ export function Comment({content, onDeleteComment}) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
